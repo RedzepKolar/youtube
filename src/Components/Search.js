02f92d0092e47238e324c25ac3886af15c3ef3a0
RKolar <bato9440@hotmail.com>
2024-11-21 10:56:51 +0100

    Added Search component

---
+++ src/Components/Search.js
@@ -0,0 +1,14 @@
+const Search = () => {
+  return (
+    <div>
+      <form>
+        <label>
+          Search:
+          <input type="text" name="search" placeholder="Search..." />
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
